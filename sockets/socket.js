96ea76c02491285f48557f4d82d56be1ea0ff0d3
Jaime Perez Frias <jperezfrias33@gmail.com>
2020-12-07 17:07:21 -0500

    Cambios sobre bandas

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,13 +1,38 @@
 const { io } = require('../index');
+const Band = require('../models/band');
+const Bands = require('../models/bands');
+
+const bands = new Bands();
 
 // Mensajes de sockets
 io.on('connection', client => {
     console.log('Cliente conectado');
+
+    client.emit('active-bands', bands.getBands());
+
     client.on('disconnect', () => {
         console.log('Cliente desconectado');
     });
-    client.on('mensaje', (payload) => {
-        console.log('Mensaje', payload['nombre']);
-        io.emit('mensaje', { admin: 'Nuevo mensaje' });
+
+    client.on('nuevo-mensaje', (payload) => {
+        // Emite a todos
+        //io.emit('nuevo-mensaje', payload);
+        // Emite a todos menos al que lo emitió
+        client.broadcast.emit('nuevo-mensaje', payload);
+    });
+
+    client.on('vote-band', (payload) => {
+        bands.voteBand(payload.id);
+        io.emit('active-bands', bands.getBands()); //Envia los cambios a todo el servidor
+    });
+
+    client.on('add-band', (payload) => {
+        bands.addBand(new Band(payload.name));
+        io.emit('active-bands', bands.getBands());
+    });
+
+    client.on('delete-band', (payload) => {
+        bands.deleteBand(payload.id);
+        io.emit('active-bands', bands.getBands());
     });
 });
